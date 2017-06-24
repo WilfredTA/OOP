c529656ce34f3544d592c39df8614171f5922953
Tannr Allard <rockinghamtannr@gmail.com>
2017-06-24 03:25:57 +0000

    beginning TTT

---
+++ lesson5/TTT_spike.rb
@@ -0,0 +1,37 @@
+class Board
+   def initialize 
+       #model the grid as 3 X 3 
+       # - Which data structures to use?
+   end
+end
+
+class Square 
+   def initialize 
+      #Occupied or not occupied?
+      # Occupied by which player?
+   end
+end
+
+class Player 
+   def initialize 
+      # Symbol associated with Player object to keep track of which symbols are player's
+   end
+   
+   def mark 
+       
+   end
+   
+   def play
+   
+   end
+    
+end
+
+class TTTGame
+   def play
+       
+   end
+end
+
+game = TTTGame.new
+game.play
